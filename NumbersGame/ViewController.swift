3a47678c0786f664956aaef97a87edf6b65e52c2
MasahiroToyooka <ehara2481@gmail.com>
2019-08-05 20:23:09 +0900

    キーボードを隠す処理

--- NumbersGame/ViewController.swift
+++ NumbersGame/ViewController.swift
@@ -19,10 +19,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        inputTextField.delegate = self
+        answerLogTextView.isUserInteractionEnabled = false
     }
 
     @IBAction func decideButton(_ sender: Any) {
+        
+        answerLabel.text = inputTextField.text
+        inputTextField.endEditing(true)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let inputTextField = inputTextField {
+            answerLabel.text = inputTextField.text
+        } else {
+            
+        }
+        self.view.endEditing(true)
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    // 改行ボタンを押した時の処理
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        answerLabel.text = textField.text
+        
+        // キーボードを隠す
+        textField.resignFirstResponder()
+        return true
     }
     
 }
