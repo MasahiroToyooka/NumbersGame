203831d0b5f885f0da084053e9ac5712f1d84832
MasahiroToyooka <ehara2481@gmail.com>
2019-08-05 21:43:04 +0900

    改行テキストが入力されたら改行する処理

--- NumbersGame/ViewController.swift
+++ NumbersGame/ViewController.swift
@@ -16,27 +16,36 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var answerLogTextView: UITextView!
     
+    var trueNum: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         inputTextField.delegate = self
         answerLogTextView.isUserInteractionEnabled = false
+        
+        resetTrueNum()
+        
     }
+    
 
     @IBAction func decideButton(_ sender: Any) {
         
+        answerLogTextView.text = answerLogTextView.text + "\(inputTextField.text ?? "")\n"
         answerLabel.text = inputTextField.text
         inputTextField.endEditing(true)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let inputTextField = inputTextField {
-            answerLabel.text = inputTextField.text
-        } else {
-            
-        }
         self.view.endEditing(true)
+    }
+    
+    func checkInputNum(num: Int) {
+        
+    }
+    
+    fileprivate func resetTrueNum() {
+        trueNum = Int.random(in: 0...100)
     }
 }
 
